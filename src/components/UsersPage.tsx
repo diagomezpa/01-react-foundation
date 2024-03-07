cfe2393fcf486bc66922d2be038c16b8e2a6d80d
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-03-07 15:53:47 -0500

    seccion 2 video 19 establecer tipo

--- src/components/UsersPage.tsx
+++ src/components/UsersPage.tsx
@@ -1,35 +1,45 @@
-import axios from "axios"
-import { useEffect } from "react"
+import axios from 'axios';
+import { useEffect } from 'react';
+import { ReqResUserListResponse } from '../interfaces';
 
+const loadUsers = async () => {
+    try {
+        const { data } = await axios.get<ReqResUserListResponse>(
+            'https://reqres.in/api/users?page=2'
+        );
+        return data.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
 
 export const UsersPage = () => {
-
- useEffect(()=> {
-    axios.get('https://reqres.in/api/users?page=2')
-    .then(resp => console.log(resp.data.total));
-    // fetch('https://reqres.in/api/users?page=2')
-    // .then(resp => resp.json())
-    // .then(data => console.log(data) );
- })    
-  return (
-    <>
-        <h3>usuarios:</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th>Avatar</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>avatar</td>
-                    <td>nombre</td>
-                    <td>email</td>
-                </tr>
-            </tbody>
-        </table>
-    </>
-  )
-}
+    useEffect(() => {
+        loadUsers().then(users => console.log(users))
+        // fetch('https://reqres.in/api/users?page=2')
+        // .then(resp => resp.json())
+        // .then(data => console.log(data) );
+    });
+    return (
+        <>
+            <h3>usuarios:</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>avatar</td>
+                        <td>nombre</td>
+                        <td>email</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+};
