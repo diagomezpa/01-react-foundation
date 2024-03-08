c7ff915f14d5f794424ad76b51cc80cc28cc41a7
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-03-08 12:12:20 -0500

    seccion 2 video 24 Envío y valores del formulario

--- src/components/FormsPage.tsx
+++ src/components/FormsPage.tsx
@@ -7,25 +7,36 @@ type FormInputs = {
 }
 export const FormsPage = () => {
 
-  const {register} = useForm<FormInputs>({
+  const {register , handleSubmit, formState , watch} = useForm<FormInputs>({
     defaultValues:{
         email:'diagomezpa@gmail.com',
         password:'Abc123'
 
     }
-  })
+  });
+
+  const onSubmit = (myForm: FormInputs) => {
+
+  }
+
+  console.log(watch('email'));
 
   return (
    <>
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
         <h3>Formularios</h3>
         <div style={{display:'flex', flexDirection:'column'}}>
-            <input type="text" placeholder='Email' {...register('email')}/>
+            <input type="text" placeholder='Email' {...register('email', {required:true})}/>
             <input type='text' placeholder='password' {...register('password')}/>
-
+           
             <button type="submit"> Ingresar</button>
         </div>
     </form>
+    
+    <pre>
+        {JSON.stringify(formState,null,2)}
+    </pre>
+
    </>
   )
 }
